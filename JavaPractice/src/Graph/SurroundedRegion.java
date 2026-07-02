8ba4c4bf554377e4433797b3f07e75258a60b18c
Nidhi Kumari <nsinha7295@gmail.com>
2026-07-02 17:59:11 +0530

    graph q content

--- JavaPractice/src/Graph/SurroundedRegion.java
+++ JavaPractice/src/Graph/SurroundedRegion.java
@@ -1,4 +1,60 @@
 package Graph;
 
 public class SurroundedRegion {
+    public void solve(char[][] board) {
+
+        int n = board.length;
+        int m = board[0].length;
+
+        //find all edges 'O' and make them #
+        for(int i=0;i<n;i++){ //1st column
+            if(board[i][0] == 'O'){
+                dfs(board, n, m, i, 0);
+            }
+        }
+        for(int i=0;i<n;i++){ //last column
+            if(board[i][m-1] == 'O'){
+                dfs(board, n, m, i, m-1);
+            }
+        }
+        for(int j=0;j<m;j++){ //1st row
+            if(board[0][j] == 'O'){
+                dfs(board, n, m, 0, j);
+            }
+        }
+        for(int j=0;j<m;j++){ //last row
+            if(board[n-1][j] == 'O'){
+                dfs(board, n, m, n-1, j);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }else{
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int n, int m, int i, int j){
+
+        board[i][j] = '#';
+        int[] row = {0,-1,1,0};
+        int[] col = {1,0,0,-1};
+        for(int k=0;k<4;k++){
+            int nrow = i + row[k];
+            int ncol = j + col[k];
+            if(isValid(n, m, nrow, ncol) && board[nrow][ncol] == 'O'){
+                dfs(board,n, m, nrow, ncol);
+            }
+        }return;
+
+    }
+
+    public boolean isValid(int n, int m, int i, int j){
+        return i < 0 || i >= n || j < 0 || j >= m ? false : true;
+    }
 }
--- JavaPractice/src/Graph/TopoSortBFS.java
+++ JavaPractice/src/Graph/TopoSortBFS.java
@@ -1,4 +1,52 @@
 package Graph;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TopoSortBFS {
+    public static void main(String[] args) {
+        int V = 4, E = 3, edges[][] = {{3, 0}, {1, 0}, {2, 0}};
+        System.out.println(topoSort(V, edges));
+    }
+    public static ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int[] indegree = new int[V];
+
+        for(int i=0;i<V;i++){
+            ArrayList<Integer> inner = new ArrayList<>();
+            adj.add(inner);
+        }
+        for(int[] edge:edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            indegree[v]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0;i<V;i++){
+            if(indegree[i] == 0)
+                q.offer(i);
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.poll();
+            result.add(node);
+
+            for(int j=0;j<adj.get(node).size();j++){
+                int neigh = adj.get(node).get(j);
+                indegree[neigh]--;
+
+                if(indegree[neigh] == 0){
+                    q.offer(neigh);
+                }
+            }
+        }
+
+        return result;
+    }
 }
